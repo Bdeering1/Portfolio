8f87f83ed8a791bc736df290d201cdf1947fe4aa
Bryn Deering <bdeering@uoguelph.ca>
2021-09-03 19:03:54 -0400

    Cleaned up ScrollContainer class and implemented horizontal react-spring scrolling

--- src/components/ScrollContainer.tsx
+++ src/components/ScrollContainer.tsx
@@ -1,58 +1,49 @@
 import React, { useEffect, useState, useRef } from 'react';
-import { useSpring, animated } from 'react-spring';
+import { useSpring, animated, config } from 'react-spring';
 import { throttle } from 'lodash';
 
 import { pauseScroll } from '../utils/scrolling';
 
 export default function ScrollContainer( props : any ) {
     const ref = useRef(null);
-    let scrollAreas : number[] = [];
-
+    const [scrollDest, setScrollDest] = useState(0);
+    const [scrollDestX, setScrollDestX] = useState([]);
 
     useEffect(() => {
         scrollInit();
         pauseScroll('main', 2000);
     }, [])
 
-    function handleScroll() {
-        let el = ref.current;
-        if (!el.hasAttribute('data-scrolled') && el.scrollTop === scrollAreas[1]) {
-            document.querySelector<HTMLElement>('.about-rect').style.animationPlayState = 'running';
-            pauseScroll('main', 1000);
-            setTimeout(() => {
-                document.querySelector<HTMLElement>('.about-title').style.opacity = '1'
-                document.querySelector<HTMLElement>('.about-desc').style.opacity = '1'
-                document.querySelector<HTMLElement>('.stack-callout').style.opacity = '1'
-            }, 250);
-            el.setAttribute('data-scrolled', '');
-        } if (el.scrollTop > scrollAreas[2]) {
-            pauseScroll('projects', 800);
-        }
-    }
-
     function scrollInit() {
+        for (let i = 0; i < props.children.length; i++) {
+            setScrollDestX(arr => ([...arr, null]));
+        }
         const el = ref.current;
 
-        //Getting bounds of scroll containers
-        let scrollLeft : number[][] = []; //horizontal scroll containers (if any)
+        let scrollPtsY : number[] = []; /* vertical scroll containers */
+        let scrollPtsX : number[][] = []; /* horizontal scroll containers (if any) */
         for (let i = 0; i < el.children.length; i++) {
             let section = el.children[i];
-            scrollAreas[i] = Math.round(section.getBoundingClientRect().top);
-            scrollLeft[i] = [null];
-            if (section.querySelector('[data-scrollable]')) {
+            scrollPtsY[i] = Math.round(section.getBoundingClientRect().top);
+            scrollPtsX[i] = [null];
+            if (section.hasAttribute('data-scroll-x')) {
                 for (let j = 0; j < section.children.length; j++) {
-                    scrollLeft[i][j] = Math.round(section.children[j].getBoundingClientRect().left);
+                    scrollPtsX[i][j] = Math.round(section.children[j].getBoundingClientRect().left);
+                    setScrollDestX(arr => [...arr.slice(0,i), 0, ...arr.slice(i+1)]);
                 }
             }
         }
+        /* additional elements are added on either to create 'infinite' scroll effect,
+             this ensures scrolling starts at the intended first element */
         document.getElementById('projects').scrollLeft = window.innerWidth;
-    
+        setScrollDestX(arr => [...arr.slice(0,3), window.innerWidth, ...arr.slice(3+1)]);
     
         /* Scroll Events
-        -handles scroll restrictions on load and small scroll behaviour fixes
+            -handles scroll restrictions on load and scroll behaviour fixes
+            -triggers on react-spring scrolling
         */
         el.addEventListener("scroll", throttle(() => {
-            if (!el.hasAttribute('data-scrolled') && el.scrollTop === scrollAreas[1]) {
+            if (!el.hasAttribute('data-scrolled') && el.scrollTop === scrollPtsY[1]) {
                 document.querySelector<HTMLElement>('.about-rect').style.animationPlayState = 'running';
                 pauseScroll('main', 1000);
                 setTimeout(() => {
@@ -61,7 +52,7 @@ export default function ScrollContainer( props : any ) {
                     document.querySelector<HTMLElement>('.stack-callout').style.opacity = '1'
                 }, 250);
                 el.setAttribute('data-scrolled', '');
-            } if (el.scrollTop > scrollAreas[2]) {
+            } if (el.scrollTop > scrollPtsY[2]) {
                 pauseScroll('projects', 800);
             }
         }, 100));
@@ -72,70 +63,80 @@ export default function ScrollContainer( props : any ) {
         */
         window.addEventListener('keydown', (e) => {
             if (el.hasAttribute('data-paused')) return;
-            let scrollArea : number;
-            for (let i = 0; i < scrollAreas.length; i++) { //determine user's current vertical scroll area
-                if (el.scrollTop == scrollAreas[i]) {
-                    scrollArea = i;
+            let scrollAreaY : number;
+            for (let i = 0; i < scrollPtsY.length; i++) {
+                if (el.scrollTop == scrollPtsY[i]) {
+                    /* user's current vertical scroll */
+                    scrollAreaY = i;
                 }
             }
-            if (scrollArea === undefined) return;
+            /* prevents weird behaviour on partial scroll */
+            if (scrollAreaY === undefined) return;
     
-            if (e.code === "ArrowLeft") {
-                let section = el.children[scrollArea];
-                if (!section.querySelector('[data-scrollable]') || section.scrollLeft === 0) return;
-                for (let i = 0; i < scrollLeft[scrollArea].length; i++) {
-                    if (section.scrollLeft === scrollLeft[scrollArea][i]) {
-                        section.scrollTo({
-                            left: scrollLeft[scrollArea][i - 1],
+            let scrollDir : string;
+            let loopStart : number;
+            let loopEnd : number;
+            switch(e.code) {
+                case "ArrowLeft":
+                    if (scrollPtsX[scrollAreaY] === null) break;
+                    scrollDir =  "horizontal";
+                    loopStart = 1;
+                    loopEnd = scrollPtsX[scrollAreaY].length;
+                    break;
+                case "ArrowRight":
+                    if (scrollPtsX[scrollAreaY] === null) break;
+                    scrollDir =  "horizontal";
+                    loopStart = 0;
+                    loopEnd = scrollPtsX[scrollAreaY].length - 1;
+                    break;
+                case "ArrowUp":
+                    if (scrollAreaY === 0) break;
+                    scrollDir = "vertical";
+                    break;
+                case "ArrowDown":
+                    if (scrollAreaY === scrollPtsY.length - 1) break;
+                    scrollDir = "vertical";
+                    break;
+                default:
+                    return;
+            }
+
+            if (scrollDir === "horizontal") {
+                let section = el.children[scrollAreaY];
+                for (let i = loopStart; i < loopEnd; i++) {
+                    if (section.scrollLeft === scrollPtsX[scrollAreaY][i]) {
+                        let scrollX = scrollPtsX[scrollAreaY][i + (e.code === "ArrowLeft" ? -1 : 1)];
+                        setScrollDestX(arr => [...arr.slice(0,scrollAreaY), scrollX, ...arr.slice(scrollAreaY+1)]);
+                        /* section.scrollTo({
+                            left: scrollPtsX[scrollAreaY][i + (e.code === "ArrowLeft" ? -1 : 1)],
                             behavior: 'smooth'
-                        })
+                        }); */
+                        break;
                     }
                 }
-            } else if (e.code === "ArrowRight") {
-                let section = el.children[scrollArea];
-                if (!section.querySelector('[data-scrollable]')
-                || section.scrollLeft === scrollLeft[scrollArea][scrollLeft[scrollArea].length - 1]) return;
-                for (let i = 0; i < scrollLeft[scrollArea].length; i++) {
-                    if (section.scrollLeft === scrollLeft[scrollArea][i]) {
-                        section.scrollTo({
-                            left: scrollLeft[scrollArea][i + 1],
-                            behavior: 'smooth'
-                        })
-                    }
-                }
-            } else if (e.code === "ArrowUp") {
-                if (scrollArea === 0) return;
-                el.scrollTo({
-                    top: scrollAreas[scrollArea - 1],
-                    behavior: 'smooth'
-                })
-            } else if (e.code === "ArrowDown") {
-                if (scrollArea === scrollAreas[scrollAreas.length] - 1) return;
-                el.scrollTo({
-                    top: scrollAreas[scrollArea + 1],
-                    behavior: 'smooth'
-                })
+            } else if (scrollDir === "vertical") {
+                ref.current.style.scrollSnapType = "none";
+                setScrollDest(scrollPtsY[scrollAreaY + (e.code === "ArrowUp" ? -1 : 1)]);
+                //ref.current.style.scrollSnapType = "y mandatory";
             }
         });
     }
 
     const { scroll } = useSpring({
-        scroll: 1000,
-        from: { scroll: 0},
-        reset: true,
-        //reverse: flip,
-        //delay: 400,
-        //config: config.molasses,
-        //onRest: () => set(!flip),
+        scroll: scrollDest,
+        //reset: true,
+        //onScrollEnd: ref.current ? ref.current.style.scrollSnapType = "y mandatory" : null
     })
 
 
     return <animated.main
         ref={ref}
         id="main"
-        onScroll={throttle(handleScroll, 100)}
-        //scrollTop={scroll}
+        scrollTop={scroll}
     >
-        {props.children}
+        {React.Children.map(
+            props.children,
+            (child, idx) => React.cloneElement(child, scrollDest[idx] !== null ? {scrollX: scrollDestX[idx]} : {})
+        )}
     </animated.main>
 }
--- src/sections/Projects.tsx
+++ src/sections/Projects.tsx
@@ -1,37 +1,58 @@
-import React from 'react';
-import ProjectCard from '../components/ProjectCard';
+import React, { useEffect, useState } from 'react';
+import { useSpring, animated } from 'react-spring';
 import { throttle } from 'lodash';
+
+import ProjectCard from '../components/ProjectCard';
 
 const projects = require('../../data/projects.json');
 
 interface ProjectsProps {
     mobileView : boolean,
     darkMode : boolean,
+    scrollX? : number
 }
 
 export default function Projects(props : ProjectsProps) {
+    const [blinkScroll, setBlinkScroll] = useState(false);
+    const [scrollX,  setScrollX] = useState(0);
+
+    const { scroll } = useSpring({
+        scroll: scrollX,
+        immediate: blinkScroll,
+        //from: {scroll: ref.current ? ref.current.scrollLeft : 0}
+        //reset: true,
+        onRest: () => {blinkScroll ? setBlinkScroll(false) : null}
+    })
+
+    useEffect(() => {
+        setScrollX(props.scrollX);
+    }, [props.scrollX])
+
     const handleScroll = (e : any) => {
-        let element = e.target;
-        if (!element) return;
-        if (element.scrollLeft == 0) {
-            element.scrollLeft += window.innerWidth * 3;
-        } else if (element.scrollLeft == window.innerWidth * 4) {
-            element.scrollLeft -= window.innerWidth * 3;
+        let el = e.target;
+        if (!el) return;
+        if (el.scrollLeft == 0) {
+            setBlinkScroll(true);
+            setScrollX(el.scrollLeft + window.innerWidth * 3);
+        } else if (el.scrollLeft == window.innerWidth * 4) {
+            setBlinkScroll(true);
+            setScrollX(el.scrollLeft - window.innerWidth * 3);
         }
     }
 
     return (
-        <section
+        <animated.section
             className="projects"
             id="projects"
             onScroll={throttle(handleScroll, 7)}
-            data-scrollable={true}
+            data-scroll-x={true}
+            scrollLeft={scroll}
         >
                 <ProjectCard project={projects[projects.length - 1]} mobileView={props.mobileView} darkMode={props.darkMode}  id={0}/>
                 {projects.map((proj, idx) => (
                     <ProjectCard project={proj} mobileView={props.mobileView} darkMode={props.darkMode}  id={idx + 1} key={idx}/>
                 ))}
                 <ProjectCard project={projects[0]} mobileView={props.mobileView} darkMode={props.darkMode}  id={projects.length + 1}/>
-        </section>
+        </animated.section>
     )
 }
