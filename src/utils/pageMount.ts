177e2f0ee981e48d44ab346d34ba02ee0622b002
Bryn Deering <bdeering@uoguelph.ca>
2021-10-06 16:20:44 -0400

    Fixed issue where fonts weren't being loaded properly on build

--- src/utils/pageMount.ts
+++ src/utils/pageMount.ts
@@ -1,31 +1,54 @@
-export default function pageMount(functions : any) {
+export default function pageMount(functions: any) {
+	let newake_font = new FontFace(
+		'Newake',
+		'url(./fonts/newake.woff2), url(/fonts/newake.woff)'
+	);
+	newake_font.display = 'swap';
+	newake_font.load().then(function (loaded_font) {
+		document.fonts.add(loaded_font);
+	});
 
-    function updateDimensions() {
-        setTimeout(() => { //fixes issue with rotation on mobile
-            let innerHeight = window.innerHeight || document.documentElement.clientHeight || document.body.clientHeight;
-            document.documentElement.style.setProperty('--inner-height', `${innerHeight}px`);
-            functions.setMobileView(window.matchMedia("(max-width: 768px)").matches)
-        }, 400);
-    }
+	function updateDimensions() {
+		setTimeout(() => {
+			//fixes issue with rotation on mobile
+			let innerHeight =
+				window.innerHeight ||
+				document.documentElement.clientHeight ||
+				document.body.clientHeight;
+			document.documentElement.style.setProperty(
+				'--inner-height',
+				`${innerHeight}px`
+			);
+			functions.setMobileView(window.matchMedia('(max-width: 768px)').matches);
+		}, 400);
+	}
 
-    function updateTheme() {
-        functions.setDarkMode(window.matchMedia("(prefers-color-scheme: dark)").matches);
-    }
+	function updateTheme() {
+		functions.setDarkMode(
+			window.matchMedia('(prefers-color-scheme: dark)').matches
+		);
+	}
 
-    window.matchMedia("(prefers-color-scheme: dark)").addEventListener("change", updateTheme);
-    window.addEventListener('resize', updateDimensions);
-    window.addEventListener('orientationchange', updateDimensions);
-    window.onblur = () => {document.title = 'Bryn Deering'};
-    window.onfocus = () => {
-			document.title = 'Portfolio';
-		};
+	window
+		.matchMedia('(prefers-color-scheme: dark)')
+		.addEventListener('change', updateTheme);
+	window.addEventListener('resize', updateDimensions);
+	window.addEventListener('orientationchange', updateDimensions);
+	window.onblur = () => {
+		document.title = 'Bryn Deering';
+	};
+	window.onfocus = () => {
+		document.title = 'Portfolio';
+	};
 
-    updateDimensions();
-    updateTheme();
+	updateDimensions();
+	updateTheme();
 
-    return function pageUnmount() {
-        window.removeEventListener('resize', updateDimensions);
-        window.removeEventListener('orientationchange', updateDimensions);
-        window.matchMedia("(prefers-color-scheme: dark)").removeEventListener("change", updateTheme);
-    }
+	return function pageUnmount() {
+		window.removeEventListener('resize', updateDimensions);
+		window.removeEventListener('orientationchange', updateDimensions);
+		window
+			.matchMedia('(prefers-color-scheme: dark)')
+			.removeEventListener('change', updateTheme);
+	};
 }
